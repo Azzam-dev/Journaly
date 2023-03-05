94b972b0e1c5c57bc74d5163398726443add4760
Azzam R Alrashed <azzam.rar@gmail.com>
2023-03-05 19:38:31 +0300

    worked on login alert

--- lib/login.dart
+++ lib/login.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({
     super.key,
   });
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  bool correctEntry = true;
+  String username = "";
+  String password = "";
+
+  checkUsernamePassword() {
+    setState(() {
+      if (username == "" || password == "") {
+        correctEntry = false;
+      } else {
+        correctEntry = true;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,8 +32,9 @@ class Login extends StatelessWidget {
           height: 30,
           width: 100,
           margin: EdgeInsets.all(10),
-          color: Colors.white,
+          color: correctEntry ? Colors.white : Colors.red,
           child: TextField(
+            onChanged: (value) => {username = value},
             decoration: InputDecoration(hintText: "Username"),
           ),
         ),
@@ -22,15 +42,16 @@ class Login extends StatelessWidget {
           height: 30,
           width: 100,
           margin: EdgeInsets.all(10),
-          color: Colors.white,
+          color: correctEntry ? Colors.white : Colors.red,
           child: TextField(
+            onChanged: (value) => {password = value},
             decoration: InputDecoration(hintText: 'Password'),
           ),
         ),
         Text('Reset password'),
         ElevatedButton(
             style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
-            onPressed: () => {print("Sign in")},
+            onPressed: checkUsernamePassword,
             child: Text("Sign in"))
       ],
     );
