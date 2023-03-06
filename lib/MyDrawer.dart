bb5956951107ffc641e93021388a4388b547ecc6
Azzam R Alrashed <azzam.rar@gmail.com>
2023-03-06 18:52:28 +0300

    worked on navigating to home, myprofile and search screens

--- lib/MyDrawer.dart
+++ lib/MyDrawer.dart
@@ -1,9 +1,36 @@
 import 'package:flutter/material.dart';
+import 'Home.dart';
+import 'MyProfile.dart';
+import 'Search.dart';
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({
     super.key,
   });
+
+  void openHomeScreen(BuildContext context) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => Home(),
+        ));
+  }
+
+  void openProfileScreen(BuildContext context) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => MyProfile(),
+        ));
+  }
+
+  void openSearchScreen(BuildContext context) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => Search(),
+        ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,25 +38,19 @@ class MyDrawer extends StatelessWidget {
       child: ListView(
         children: [
           ListTile(
-            onTap: () {
-              print("did tap Home");
-            },
+            onTap: () => openHomeScreen(context),
             leading: Icon(Icons.home),
             title: Text("Home"),
             subtitle: Text("this is the Home page"),
           ),
           ListTile(
-            onTap: () {
-              print("did tap Profile");
-            },
+            onTap: () => openProfileScreen(context),
             leading: Icon(Icons.person),
             title: Text("Profile"),
             subtitle: Text("this is your Profile page"),
           ),
           ListTile(
-            onTap: () {
-              print("did tap Search");
-            },
+            onTap: () => openSearchScreen(context),
             leading: Icon(Icons.search),
             title: Text("Search"),
             subtitle: Text("this is the Search page"),
